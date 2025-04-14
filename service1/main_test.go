8df9a4887dc5df10baecbe1c908bbdee7c37509f
elliehashemi <ellie.hashemi@redspace.com>
2025-04-14 09:02:31 -0300

    fix: changed in service1 tests

--- service1/main_test.go
+++ service1/main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestExample(t *testing.T) {
-	t.Log("✅ Service1 test passed")
+	t.Log("✅ Service1 test passed!")
 }
 func TestExample2(t *testing.T) {
-	t.Log("✅ Service1 test2 passed")
+	t.Log("✅ Service1 test2 passed!")
 }
