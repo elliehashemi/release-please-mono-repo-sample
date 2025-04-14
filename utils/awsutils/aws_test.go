f06484f2c4d8fdee34ac14b39401d6fd8993c054
elliehashemi <ellie.hashemi@redspace.com>
2025-04-14 09:15:49 -0300

    fix: added -v to ci tests

--- utils/awsutils/aws_test.go
+++ utils/awsutils/aws_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestExample(t *testing.T) {
-	t.Log("✅ AWS test passed")
+	t.Log("✅ AWS test passed!")
 }
 func TestExample2(t *testing.T) {
 	t.Log("✅ AWS test2 passed")
