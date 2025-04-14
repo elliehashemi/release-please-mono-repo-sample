32816f5784cae697343543c07dca517540d5247d
elliehashemi <ellie.hashemi@redspace.com>
2025-04-14 09:12:36 -0300

    feat: added tests in utils

--- utils/awsutils/aws_test.go
+++ utils/awsutils/aws_test.go
@@ -5,3 +5,6 @@ import "testing"
 func TestExample(t *testing.T) {
 	t.Log("✅ AWS test passed")
 }
+func TestExample2(t *testing.T) {
+	t.Log("✅ AWS test2 passed")
+}
--- utils/gcputils/gcp_test.go
+++ utils/gcputils/gcp_test.go
@@ -5,3 +5,6 @@ import "testing"
 func TestExample(t *testing.T) {
 	t.Log("✅ GCP test passed")
 }
+func TestExample2(t *testing.T) {
+	t.Log("✅ GCP test2 passed")
+}
